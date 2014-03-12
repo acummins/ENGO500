156898c9f3210cf6aa8bdb2a1fc57c4c4c0260ce
acummins <alex13@shaw.ca>
2014-03-12 16:07:09 -0600

    update local

--- Sensor_prototype/PostObsPIR.py
+++ Sensor_prototype/PostObsPIR.py
@@ -2,11 +2,7 @@
 # PostObsPIR.py
 # an attempt at mashing up existing scripts (oh boy!)
 # and cleaning them up while we're at it
-# Last Edited: march 8 2014 AC
-
-ctl=open('/home/pi/ENGO500/Sensor_prototype/checkthinglocation.txt','a+')
-csl=open('/home/pi/ENGO500/Sensor_prototype/checksensorlocation.txt','a+')
-
+# Last Edited: march 12 2014 AC
 
 import time, datetime, requests, json, RPi.GPIO as GPIO
 ID = " RPi 1 "
@@ -24,69 +20,72 @@ class data:
 
     def initRequest(self):
         n=1
-        c = ctl.read(n)
-        if (c == 'h'):
-            print("Things exists")
-            thing_location = ctl.readline()
-            self.thing_location = 'h' + thing_location
-            print(self.thing_location)
-            r2 = requests.get(self.thing_location)
-            sr2 = str(r2)
-            print(sr2)
-            print('check r2')
-            if (sr2 == '<Response [404]>'):
-                ctl.seek(0)
-                ctl.truncate()
-                csl.seek(0)
-                csl.truncate()
-                print(sr2)
-                print("Server has reset. Links have been changed, please restart.")
-                exit()
-            elif (sr2 == '<Response [200]>'):
-                print(sr2)
-                print('this is 200')
-            else:
-                print("rcode sting match failed")
-            self.sensors_location = csl.readline()
-            #print(self.sensors_location)
-            rcode = requests.get(self.sensors_location)
-            response = rcode.json()
-            self.sensorID = response['ID']
-            print(self.sensorID)   
-            
-        else:
-            print("Thing does not exist.  Create thing.")
-            url = self.rootURI() + 'Things'
-            payload = {'Description': 'This is the real TurboCat (Raspberry Pi)',
-                'Datastreams':
-                    [{'Description': 'This is a datastream for measuring people traffic'},
-                        {'Description': 'This is a second datastream'}]}
-            headers = {'content-type': 'application/json'}
+        with open('/home/pi/ENGO500/Sensor_prototype/checkthinglocation.txt', 'a+') as ctl:
+            with open('/home/pi/ENGO500/Sensor_prototype/checksensorlocation.txt','a+') as csl:
+                c = ctl.read(n)
+                if (c == 'h'):
+                    print("Things exists")
+                    thing_location = ctl.readline()
+                    self.thing_location = 'h' + thing_location
+                    print(self.thing_location)
+                    r2 = requests.get(self.thing_location)
+                    sr2 = str(r2)
+                    print(sr2)
+                    print('check r2')
+                    if (sr2 == '<Response [404]>'):
+                        ctl.seek(0)
+                        ctl.truncate()
+                        csl.seek(0)
+                        csl.truncate()
+                        print(sr2)
+                        print("Server has reset. Links have been changed, please restart.")
+                        #LED HOLD
+                        GPIO.output(GPIO_LED, True)
+                        exit()
+                    elif (sr2 == '<Response [200]>'):
+                        print(sr2)
+                        print('this is 200')
+                    else:
+                        print("rcode sting match failed")
+                    self.sensors_location = csl.readline()
+                    rcode = requests.get(self.sensors_location)
+                    response = rcode.json()
+                    self.sensorID = response['ID']
+                    print(self.sensorID)   
+                    
+                else:
+                    print("Thing does not exist.  Create thing.")
+                    url = self.rootURI() + 'Things'
+                    payload = {'Description': 'This is the real TurboCat (Raspberry Pi)',
+                        'Datastreams':
+                            [{'Description': 'This is a datastream for measuring people traffic'},
+                                {'Description': 'This is a second datastream'}]}
+                    headers = {'content-type': 'application/json'}
 
-            r = requests.post(url, data=json.dumps(payload), headers=headers)
-            ## ERROR CHECKING?! status code
-            thing_location = r.headers['location']
-            self.thing_location = thing_location
-            ctl.write(thing_location)
+                    r = requests.post(url, data=json.dumps(payload), headers=headers)
+                    ## ERROR CHECKING?! status code
+                    thing_location = r.headers['location']
+                    self.thing_location = thing_location
+                    ctl.write(thing_location)
 
-            url3 = self.rootURI() + 'Sensors'
-            payload2 = {'Metadata': 'This is a PIR sensor'}
-            rsensor = requests.post(url3, data=json.dumps(payload2), headers=headers)
-            self.sensors_location = rsensor.headers['location']
+                    url3 = self.rootURI() + 'Sensors'
+                    payload2 = {'Metadata': 'This is a PIR sensor'}
+                    rsensor = requests.post(url3, data=json.dumps(payload2), headers=headers)
+                    self.sensors_location = rsensor.headers['location']
 
-            csl.write(self.sensors_location)
-            rsensor = requests.get(self.sensors_location)
-            response = rsensor.json()
-            self.sensorID = response['ID']            
+                    csl.write(self.sensors_location)
+                    rsensor = requests.get(self.sensors_location)
+                    response = rsensor.json()
+                    self.sensorID = response['ID']            
 
-        #check txt files
         return thing_location
 
-    def sendObs(self, obs):
+    def sendObs(self, obs, dsnum):
         url2 = self.thing_location + '/Datastreams'
         r2 = requests.get(url2)
         response2 = r2.json()
-        datastreamID = response2['Datastreams'][0]['ID']
+        datastreamID = response2['Datastreams'][dsnum]['ID']
+        print(datastreamID)
         urlOBS = self.rootURI() + 'Observations'
         headers = {'content-type': 'application/json'}
         payloadOBS = {'Time':time.strftime('%FT%T%z'),
@@ -113,15 +112,23 @@ GPIO.setmode(GPIO.BCM)
 # LED GPIO
 GPIO_LED = 4
 GPIO.setup(GPIO_LED,GPIO.OUT)
-
-# Define GPIO to use on Pi
+# For PhotoInt
+GPIO.setup(17,GPIO.IN)
+# For PIR
 GPIO_PIR = 18
-
-print "PIR Module Test (CTRL-C to exit)"
-
-# Set pin as input
 GPIO.setup(GPIO_PIR,GPIO.IN)      # Echo
 
+if GPIO.input(17):
+  print('Port 17 is 1/GPIO.HIGH/True')
+else:
+  print('Port 17 is 0/GPIO.LOW/False')
+if GPIO.input(18):
+  print('Port 18 is 1/GPIO.HIGH/True')
+else:
+  print('Port 18 is 0/GPIO.LOW/False')
+
+Switch_State = 1
+PSS = 1
 Current_State  = 0
 Previous_State = 0
 
@@ -133,7 +140,7 @@ try:
   while GPIO.input(GPIO_PIR)==1:
     Current_State  = 0    
 
-  print "  Ready"     
+  print "  Ready GO"     
     
   # Loop until users quits with CTRL-C
   while True :
@@ -144,18 +151,31 @@ try:
     if Current_State==1 and Previous_State==0:
       # PIR is triggered
       print "  Motion detected!"
-      thing1.sendObs(1)
+      thing1.sendObs(1,0)
       GPIO.output(GPIO_LED, True)
       # Record previous state
       Previous_State=1
     elif Current_State==0 and Previous_State==1:
       # PIR has returned to ready state
       print "  Ready"
-      thing1.sendObs(0)
+      thing1.sendObs(0,0)
       GPIO.output(GPIO_LED, False)
       Previous_State=0
-      
-    # Wait for 10 milliseconds
+    else:
+        continue
+
+
+    #------PhotoInt
+    Switch_State = GPIO.input(17)
+    if Switch_State == 0 and PSS == 1:
+        print("SWITCH STATE IS ZERO")
+        thing1.sendObs(1,1)
+        PSS = 0
+    elif Switch_State == 1 and PSS == 0:
+        print('Switch is reset')
+        thing1.sendObs(0,1)
+        PSS = 1
+
     time.sleep(0.01)      
       
 except KeyboardInterrupt:
